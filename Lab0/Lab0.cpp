cc0ed347abe454611714a5744c18dd78d1635f7f
Noah Daugherty <noahpd@gatech.edu>
2017-05-19 09:26:05 -0400

    Lab0 Code

---
+++ Lab0/Lab0.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+
+int main()
+{
+
+	std::cout << "This (\") is a double quote.\n";
+	std::cout << "This (\') is a single quote.\n";
+	std::cout << "This (\\) is a backslash.\n";
+	std::cout << "This (/) is a forward slash.\n\n";
+
+	return 0;
+
+}
